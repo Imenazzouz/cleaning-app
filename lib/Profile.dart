4496071e188e7d989fcc89ccc90529ccc30bceb3
iheb2099 <iheb9b1@gmail.com>
2021-12-10 08:54:05 +0100

    services and profile

---
+++ lib/Profile.dart
@@ -0,0 +1,281 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/cupertino.dart';
+
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  final VoidCallback onClicked;
+
+  const ButtonWidget({
+    Key? key,
+    required this.text,
+    required this.onClicked,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.blueAccent,
+          textStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+          shape: const StadiumBorder(),
+          onPrimary: Colors.white,
+          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+        ),
+        child: Text(text),
+        onPressed: onClicked,
+      );
+}
+
+class NumbersWidget extends StatelessWidget {
+  const NumbersWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          buildButton(context, '5', 'Services'),
+          buildDivider(),
+          buildButton(context, '10', 'Following'),
+          buildDivider(),
+          buildButton(context, '150', 'Points'),
+        ],
+      );
+  Widget buildDivider() => const SizedBox(
+        height: 24,
+        child: VerticalDivider(),
+      );
+
+  Widget buildButton(BuildContext context, String value, String text) =>
+      MaterialButton(
+        padding: const EdgeInsets.symmetric(vertical: 4),
+        onPressed: () {},
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              value,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            const SizedBox(height: 2),
+            Text(
+              text,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      );
+}
+
+class ProfileWidget extends StatelessWidget {
+  final String imagePath;
+  final VoidCallback onClicked;
+
+  const ProfileWidget({
+    Key? key,
+    required this.imagePath,
+    required this.onClicked,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme.primary;
+
+    return Center(
+      child: Stack(
+        children: [
+          buildImage(),
+          Positioned(
+            bottom: 0,
+            right: 4,
+            child: buildEditIcon(color),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildImage() {
+    final image = NetworkImage(imagePath);
+
+    return ClipOval(
+      child: Material(
+        color: Colors.transparent,
+        child: Ink.image(
+          image: image,
+          fit: BoxFit.cover,
+          width: 128,
+          height: 128,
+          child: InkWell(onTap: onClicked),
+        ),
+      ),
+    );
+  }
+
+  Widget buildEditIcon(Color color) => buildCircle(
+        color: Colors.white,
+        all: 3,
+        child: buildCircle(
+          color: color,
+          all: 8,
+          child: const Icon(
+            Icons.edit,
+            color: Colors.white,
+            size: 20,
+          ),
+        ),
+      );
+
+  Widget buildCircle({
+    required Widget child,
+    required double all,
+    required Color color,
+  }) =>
+      ClipOval(
+        child: Container(
+          padding: EdgeInsets.all(all),
+          color: color,
+          child: child,
+        ),
+      );
+}
+
+class User {
+  final String imagePath;
+  final String name;
+  final String email;
+  final String about;
+  final bool isDarkMode;
+
+  const User({
+    required this.imagePath,
+    required this.name,
+    required this.email,
+    required this.about,
+    required this.isDarkMode,
+  });
+}
+
+AppBar buildAppBar(BuildContext context) {
+  final icon = CupertinoIcons.moon_stars;
+
+  return AppBar(
+    leading: const BackButton(),
+    backgroundColor: Colors.transparent,
+    elevation: 0,
+    actions: [
+      IconButton(
+        icon: Icon(icon),
+        onPressed: () {},
+      ),
+    ],
+  );
+}
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+        'https://scontent.ftun16-1.fna.fbcdn.net/v/t1.6435-9/64580623_1589802191151097_8349125233231265792_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=9Ur2hidDrp0AX9yK2kB&_nc_ht=scontent.ftun16-1.fna&oh=844f750920af9bf784a81e77ce1f0d79&oe=61CF8B17',
+    name: 'Yesmine Gharbi',
+    email: 'Yesmine.Gharbi@supcom.tn',
+    about: '2nd year ICT engineering student.',
+    isDarkMode: false,
+  );
+}
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    final user = UserPreferences.myUser;
+
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      appBar: buildAppBar(context),
+      body: Container(
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(40.0),
+              topRight: Radius.circular(40.0),
+            )),
+        child: ListView(
+          physics: const BouncingScrollPhysics(),
+          children: [
+            const SizedBox(height: 18),
+            ProfileWidget(
+              imagePath: user.imagePath,
+              onClicked: () async {},
+            ),
+            const SizedBox(height: 24),
+            buildName(user),
+            const SizedBox(height: 24),
+            Center(child: buildUpgradeButton()),
+            const SizedBox(height: 24),
+            const NumbersWidget(),
+            const SizedBox(height: 40),
+            buildAbout(user),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildName(User user) => Column(
+        children: [
+          Text(
+            user.name,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            user.email,
+            style: const TextStyle(color: Colors.grey),
+          )
+        ],
+      );
+
+  Widget buildUpgradeButton() => ButtonWidget(
+        text: 'Disconnect',
+        onClicked: () {},
+      );
+
+  Widget buildAbout(User user) => Container(
+        decoration: const BoxDecoration(
+            color: Colors.blueAccent,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
+              bottomLeft: Radius.circular(20.0),
+              bottomRight: Radius.circular(20.0),
+            )),
+        margin: EdgeInsets.all(48),
+        padding: const EdgeInsets.symmetric(horizontal: 58),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 15),
+            const Text(
+              'About:',
+              style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              user.about,
+              style: const TextStyle(fontSize: 15),
+            ),
+            const SizedBox(height: 15)
+          ],
+        ),
+      );
+}
