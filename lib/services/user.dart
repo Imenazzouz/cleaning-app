06e006a17a0cf760694acc8ee87af5cbfc6c2241
Imenazzouz <86930374+Imenazzouz@users.noreply.github.com>
2021-12-12 00:10:47 +0100

    iheb bhim

---
+++ lib/services/user.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<void> userSetup (String username, String email) async {
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+  FirebaseAuth auth = FirebaseAuth.instance;
+String uid = auth.currentUser!.uid.toString();
+  String email = auth.currentUser!.email.toString();
+
+users.add({'username': username,'uid': uid, 'email' : email});
+return;
+
+}
+
